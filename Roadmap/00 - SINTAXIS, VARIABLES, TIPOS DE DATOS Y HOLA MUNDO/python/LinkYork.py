803ed874f5cc4aa403c945df8748bec401a35f0a
Link York <75458985+LinkYork@users.noreply.github.com>
2024-12-27 08:06:56 -0500

    Create LinkYork.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/LinkYork.py
@@ -0,0 +1,27 @@
+# http://python.org
+
+# Comentario en una linea
+
+"""
+Esto tambien es
+un comentario
+en varias lineas
+"""
+
+my_variable = "Mi variable"
+my_variable = "Nuevo valor de mi variable"
+
+MY_CONSTANT = "Mi constante"# por convencion
+
+my_int = 5
+my_float = 1.5
+my_bool = true
+my_bool = false
+my_string = "cadena de texto"
+my_other_string = "otra cadena de texto"
+print("¡Que bola, python!")
+
+print(type(my_int))
+print(type(my_flout))
+print(type(my_bool))
+print(type(my_string))
